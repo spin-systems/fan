29836b7f525e902d5e978719a20f259b346cea53
Louis <louismmx@gmail.com>
2018-02-15 19:32:26 +0000

    Add naive locus

---
+++ log/space/ocu/src/pull-wikis.sh
@@ -0,0 +1,12 @@
+readarray repos < repo-list.txt
+for pair in "${repos[@]}"
+do
+  remoterepo="$(echo $pair | cut -d\: -f 1)"
+  shortname="$(echo $pair | cut -d\: -f 2)"
+  if [ ! -d ../w/$shortname ]; then
+    git clone git@github.com:naiveoculus/$remoterepo.wiki.git ../w/$shortname
+  else
+    echo "Pulling $shortname since local repo already exists"
+    git -C ../w/$shortname pull
+  fi
+done

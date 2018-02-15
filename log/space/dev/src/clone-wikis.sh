63defa1c5ad44363f8f232b5a28731828495efae
Louis <louismmx@gmail.com>
2018-02-15 18:42:53 +0000

    Automate wiki pull

--- log/space/dev/src/clone-wikis.sh
+++
@@ -1,7 +0,0 @@
-readarray repos < repo-list.txt
-for pair in "${repos[@]}"
-do
-  remoterepo="$(echo $pair | cut -d\: -f 1)"
-  shortname="$(echo $pair | cut -d\: -f 2)"
-  git clone git@github.com:spin-log/$remoterepo.wiki.git ../$shortname
-done
